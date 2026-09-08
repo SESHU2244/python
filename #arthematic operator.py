a463a7017b47133a3b337d99780d0d5e8a1215ed
seshadri <seshadria33@gmail.com>
2026-09-08 10:28:50 +0530

    Add files via upload

---
+++ #arthematic operator.py
@@ -0,0 +1,54 @@
+a = 10
+b = 3
+
+print("Addition:" , a + b)
+print("Subtraction:" , a - b)
+print("Multiplication:" , a * b)
+print("Division" , a / b)
+print("Floor Division:" , a // b)
+print("reminder:" , a % b)
+print("Power:" , a ** b)
+
+#simple calculetor
+a = int(input("Enter first number:"))
+b = int(input("Enter second number:"))
+
+print("Addition:" , a + b)
+print("Subtraction:" , a - b)
+print("Multiplication:" , a * b)
+print("Division:" , a / b)
+
+#identity operators
+a = None 
+
+print(a is None)
+print(a is not None)
+
+
+#bitwise operators
+a = 5
+b = 3
+
+print(a & b)
+print(a | b)
+print(a ^ b)
+
+#eletricity bill calculator
+units = int(input("Enter the number of units:"))
+
+rate = 6 
+
+bill = units * rate
+
+print("Electricity Bill:" , bill)
+
+#travel expense calculator
+travel = float(input("travel expense: "))
+food = float(input("food expense: "))
+hotel = float(input("hotel expense: "))
+
+total = travel + food + hotel
+
+print("total Expense:" , total)
+
+ 
